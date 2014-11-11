2dad9389cf1753aebc953d6dd60ff15f4a2c610f
NeLk <nelk42@gmail.com>
2014-11-11 17:20:03 +0000

    editSupplements test halfway working, retrieval details of supplement in supplements working, retrieval in editSupplement.html not working, it returns undefined'

--- e2e/pageObjects/supplements.po.js
+++ e2e/pageObjects/supplements.po.js
@@ -43,16 +43,36 @@ supplementsPage = function () {
         element(by.id('btnSaveNewSupplement')).click();
     };
 
-    me.editSupplement = function() {
-        element.all(by.repeater('supplement in supplements')).first().element(by.id('btnSupplementDetails')).click();
-        element(by.id('name')).sendKeys('EDITED');
-//        element(by.id('price')).sendKeys('11:40');
-//        element(by.id('btnSaveNewSupplement')).click();
+    me.getFirstSupplementDetails = function () {
+        var firstElement = element.all(by.repeater('supplement in supplements')).first();
+        var firstSupplementDetails = {};
+        firstSupplementDetails.sid = firstElement.element(by.binding('supplement.id')).getText();
+        firstSupplementDetails.sname = firstElement.element(by.binding('supplement.name')).getText();
+        firstSupplementDetails.sprice = firstElement.element(by.binding('supplement.price')).getText();
+        return firstSupplementDetails;
     };
 
-    me.getName = function () {
-        element(by.id('name'));
+    me.navigateToFirstSupplement = function () {
+        element.all(by.repeater('supplement in supplements')).first().element(by.id('btnSupplementDetails')).click();
     };
+
+    me.getEditedSupplementDetails = function() {
+        var supplementDetails = {};
+
+        //supplementDetails.sid = element(by.id('id'));
+        supplementDetails.sname = element(by.id('name')).getAttribute('value');
+        console.log(supplementDetails.sname)
+        supplementDetails.sprice = element(by.id('price'));
+        console.log(supplementDetails.sprice)
+
+        element(by.id('btnSaveSupplement')).click();
+    };
+
+    me.editSupplement = function (name, price) {
+        element(by.id('name')).sendKeys(name);
+        element(by.id('price')).sendKeys(price);
+        element(by.id('btnSaveNewSupplement')).click();
+    }
 
 };
 
--- e2e/specs/supplements.spec.js
+++ e2e/specs/supplements.spec.js
@@ -21,8 +21,53 @@ describe('As an owner', function() {
             expect(supplementsPage.getSubPage()).toBe('Supplements');
         });
 
+        xit('be able to add supplements', function () {
+            supplementsPage.addSupplement();
+            expect(supplementsPage.getNoOfSupplements()).toBe(supplementsBefore + 1);
+        });
+
         xit('see 1+ supplements', function () {
-            expect(supplementsPage.getNoOfSupplements()).toBeGreaterThan(1);
+            expect(supplementsPage.getNoOfSupplements()).toBeGreaterThan(0);
+        });
+
+        it('be able to access a supplement\'s details', function () {
+
+            // firstSupplements Details
+            var firstSupplement = supplementsPage.getFirstSupplementDetails();
+
+            //Navigate to First Supplement
+            supplementsPage.navigateToFirstSupplement();
+
+            // editedSupplement Details
+            var editedSupplement = supplementsPage.getEditedSupplementDetails();
+
+            // Making sure data is correct for firstSupplement
+            expect(firstSupplement.sid).toBe('Id')
+            expect(firstSupplement.sname).toBe('Name')
+            expect(firstSupplement.sprice).toBe('Price')
+
+//            // Making sure data is correct for editedSupplement
+//            expect(editedSupplement.sid).toBe('Id')
+//            expect(editedSupplement.sname).toBe('Name')
+//            expect(editedSupplement.sprice).toBe('Price')
+
+            //They should both be the same
+
+
+        });
+
+        xit('be able to change a supplement\'s details and save them', function () {
+            supplementsPage.navigateToFirstSupplement();
+
+            var editedSupplement = supplementsPage.getEditedSupplementDetails();
+
+            supplementsPage.editSupplement('Edited', 12.99);
+
+            var firstSupplement = supplementsPage.getFirstSupplementDetails();
+
+            expect(editedSupplement).not.toBe(firstSupplement);
+            expect(firstSupplement.name).toBe('Edited');
+            expect(firstSupplement.price).toBe(12.99);
         });
 
         xit('be able to delete supplements', function () {
@@ -30,18 +75,6 @@ describe('As an owner', function() {
             expect(supplementsPage.getNoOfSupplements()).toBe(supplementsBefore - 1);
         });
 
-        xit('be able to add supplements', function () {
-            supplementsPage.addSupplement();
-            expect(supplementsPage.getNoOfSupplements()).toBe(supplementsBefore + 1);
-        });
-
-        it('be able to edit supplements', function () {
-            supplementsPage.editSupplement();
-            expect(supplementsPage.getSubPage()).toBe('EDITED');
-            expect(supplementsPage.getName()).toBe('Ramon');
-
-        });
-        
     });
 
 });
